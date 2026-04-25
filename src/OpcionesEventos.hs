b01d714d74cd997e020ca53ee16ffae1fe389283
MaikelFn <maikelflores27@gmail.com>
2026-04-25 12:37:02 -0600

    Generarevento
    funciones para generar: Categorías, mes, años, día y facturas y implementación temporal de generar evento

--- src/OpcionesEventos.hs
+++ src/OpcionesEventos.hs
@@ -1,5 +1,9 @@
 module OpcionesEventos
-  ( opcionTransformacion
+  ( Evento(..)
+  , ListaIdsUsados
+  , generarFecha
+  , generarEvento
+  , opcionTransformacion
   , opcionAnalisisDatos
   , opcionAnalisisTemporal
   , opcionBusqueda
@@ -23,12 +27,32 @@ impuestoCompra = 0.13
 generarValor :: IO Double
 generarValor = randomRIO (500, 75000)
 
+generarCategoria :: IO String
+generarCategoria = do
+  indice <- randomRIO (0, length categorias - 1)
+  return (categorias !! indice)
+
+generarAño :: IO Int
+generarAño = randomRIO (2026, 2028)
+
+generarMes :: IO Int
+generarMes = randomRIO (1, 12)
+
+generarDia :: IO Int
+generarDia = randomRIO (1, 31)
+
+generarFecha :: IO String
+generarFecha  = do
+  año <- generarAño
+  mes <- generarMes
+  dia <- generarDia
+  return (show año ++ "-" ++  show mes ++ "-" ++  show dia)
 
 data Evento = Evento
   { eventoId :: Int
   , categoria :: String
   , valor :: Double
-  , timestamp :: Integer
+  , fecha :: String
   }
   deriving (Eq, Show, Read)
 
@@ -43,9 +67,28 @@ generarId usados = do
     then generarId usados
     else return candidato
 
+generarEvento :: ListaIdsUsados -> IO Evento
+generarEvento usados = do
+  nuevoId <- generarId usados
+  nuevaCategoria <- generarCategoria
+  nuevoValor <- generarValor
+  nuevaFecha <- generarFecha
+  return
+    Evento
+      { eventoId = nuevoId
+      , categoria = nuevaCategoria
+      , valor = nuevoValor
+      , fecha = nuevaFecha
+      }
+
 opcionTransformacion :: IO ()
 opcionTransformacion = do
-  putStrLn "[Pendiente] Transformacion de eventos"
+  evento <- generarEvento []
+  putStrLn "Evento generado:"
+  putStrLn ("ID: " ++ show (eventoId evento))
+  putStrLn ("Categoria: " ++ categoria evento)
+  putStrLn ("Valor: " ++ show (valor evento))
+  putStrLn ("Fecha: " ++ fecha evento)
 
 opcionAnalisisDatos :: IO ()
 opcionAnalisisDatos = do
